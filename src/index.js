0a4d81da904d6e64e855189f4eab47d39c27cbec
nishu847 <nishusharma.sharma2006@gmail.com>
2025-05-13 20:57:57 +0530

    Reorganized project structure

---
+++ src/index.js
@@ -0,0 +1,21 @@
+import dotenv from "dotenv"
+dotenv.config({path:'./env'})
+import express from "express"
+import mongoose from "mongoose"
+import connectDB from "./db/db.js"
+
+import app from "./app.js"
+
+//PORT number
+const PORT= process.env.PORT || 5000
+// database connection
+connectDB().then(()=>{
+    console.log("CONNECTED")
+    app.listen(PORT,()=>{
+        console.log(`server is listening at port ${PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log("FAILED CONNECTION: ",err)
+})
+
---
+++ src/routes/userroutes.js
@@ -0,0 +1,13 @@
+import { Router } from "express";
+import { registerUser,loginUser, forgotPassword, resetPassword, logoutUser, updateUsername } from "../controller/register.js";
+import { verifyJWT } from "../middleware/authVerify.js";
+import { getEvent } from "../controller/events.js";
+const router=Router()
+router.route("/register").post(registerUser)
+router.route("/login").post(loginUser)
+router.route("/forgotpassword").post(forgotPassword)
+router.route("/resetpassword").post(resetPassword)
+router.route("/logout").post(verifyJWT,logoutUser)
+router.put("/:userId", updateUsername);
+router.get("/:userId/events", getEvent);
+export default router
